52bc69c1a8d99552b5072b7683d968a2314e23dc
robin <robinshilohhalfon@gmail.com>
2026-09-16 11:11:08 +0300

    timedelta

--- BankNitzanim/deposit.py
+++ BankNitzanim/deposit.py
@@ -3,12 +3,12 @@
 # import random
 # import time
 # import matplotlib
-from datetime import date, timedelta
+import datetime
 import consts
 
 
 def create_deposit(checking_account: int, deposit_type: int, interest_type: int, interest_rate: float,
-                   deposit_time=int):
+                   deposit_time=int, start_date= datetime):
     """
     :param checking_account: the checking account from which the money will come
     :param deposit_type: 0 or 1 as set in consts - a singular deposit or a monthly deposit
@@ -16,9 +16,9 @@ def create_deposit(checking_account: int, deposit_type: int, interest_type: int,
 				          interest rate
 	:param interest_rate: the amount of interest that is added to the deposit once a year
 	:param deposit_time: the amount of time the money will be deposited
+	:param start_date: the day in the game
 	:return: a dictionary with the values it received
     """
-    start_date = date.today()
     initial_deposit = int(input("put in deposit amount"))
     while checking_account < initial_deposit:
         print("you don't have enough to start the deposit")
@@ -30,11 +30,15 @@ def create_deposit(checking_account: int, deposit_type: int, interest_type: int,
     return deposit
 
 
-def calc_day_difference(deposit: dict):
-    today = date.today()
+def calc_day_difference(deposit: dict, day_in_game=datetime):
+    """
+    :param deposit: the deposit
+    :param day_in_game: the day in the game
+    :return: the number of days that passed since it was opened
+    """
     start = deposit["start_date"]
-    difference = today - start
-    difference = difference.days
+    # difference = day_in_game - start
+    difference = (day_in_game - start).Days
     return difference
 
 
@@ -62,14 +66,14 @@ def add_to_deposit(deposit: dict):
     return deposit["initial_deposit"]
 
 
-def can_draw_deposit(deposit: dict):
+def can_draw_deposit(deposit: dict, day_in_game=datetime):
     """
     :param deposit: the deposit we want to draw into the checking account
-
+    :param day_in_game: the day in the game
     :return: if enough time has passed so we can draw the deposit
     :return: prints a fitting message
     """
-    if calc_day_difference(deposit) >= deposit["deposit_time"]:
+    if calc_day_difference(deposit, day_in_game) >= deposit["deposit_time"]:
         print(f"Drawing {deposit["deposit_amount"]} from deposit into checking account")
         return True
     else:
@@ -77,23 +81,25 @@ def can_draw_deposit(deposit: dict):
         return False
 
 
-def can_add_interest(deposit: dict):
+def can_add_interest(deposit: dict, day_in_game=datetime):
     """
     :param deposit: the deposit we want to add interest to
+    :param day_in_game: the day in the game
     :return:
     """
-    if (calc_day_difference(deposit)) % consts.YEAR == 0:
+    if (calc_day_difference(deposit, day_in_game)) % consts.YEAR == 0:
         return True
     else:
         return False
 
 
-def calc_interest(deposit: dict):
+def calc_interest(deposit: dict, day_in_game=datetime):
     """
     :param deposit: the deposit to calc interest from
+    :param day_in_game: the day in the game
     :return: the interest that was earned while saving
     """
-    if can_draw_deposit(deposit):
+    if can_draw_deposit(deposit, day_in_game):
         interest_earned = deposit["amount_in_deposit"] * deposit["interest_rate"] / 100
     else:
         interest_earned = 0
